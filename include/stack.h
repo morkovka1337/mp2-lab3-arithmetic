d96d50d1ba632280c876de98204cddc66c4375f7
Vladimir Loginov <morkovka1337@users.noreply.github.com>
2017-11-26 13:11:28 +0300

    complited stack with tests

--- include/stack.h
+++ include/stack.h
@@ -7,3 +7,111 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыделяться память
+
+#ifndef __STACK_H__
+#define __STACK_H__
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX_VECTOR_SIZE = 1000;
+
+// Шаблон стека
+template <class ValType>
+class Stack
+{
+protected:
+	ValType *pMem;
+	int top;
+	int Elements; // количество элементов в стеке
+public:
+	Stack(int s = 1000);
+	~Stack() { delete[] pMem; };
+	int GetElements();
+	void Push(ValType d);
+	int GetSize(){ return top; }
+	ValType Eject();
+	ValType Top();
+	void Clean();
+	bool IsEmpty();
+	bool IsFull();
+};
+
+template <class ValType>
+Stack<ValType>::Stack(int s = 1000)
+{
+	if (s<0 || s>MAX_VECTOR_SIZE)
+		throw "error"; 
+	Elements = s; 
+	pMem = new ValType[s]; 
+	top = 0; 
+}
+
+template <class ValType>
+bool Stack<ValType>::IsFull()
+{
+	bool k = (top == Elements) ? true : false;
+	return k;
+}
+
+template <class ValType>
+bool Stack<ValType>::IsEmpty()
+{
+	return (top == 0);
+}
+
+template <class ValType>
+void Stack<ValType>::Clean()
+{
+	//if (IsEmpty()) throw "stack is empty";
+	top = 0;
+}
+
+template <class ValType>
+ValType Stack<ValType>::Top()
+{ 
+	if (IsEmpty()) throw "stack is empty";
+	return pMem[top - 1];
+}
+template <class ValType>
+int Stack<ValType>::GetElements()
+{
+	return Elements;
+}
+template <class ValType>
+ValType Stack<ValType>::Eject()
+{
+	ValType temp;
+	if (IsEmpty()) throw "stack is empty";
+	if (!IsEmpty())
+	{
+		temp = pMem[top - 1];
+		top -= 1;
+	}
+	return temp;
+}
+
+template <class ValType>
+void Stack<ValType>::Push(ValType d)
+{
+	if (IsFull())
+	{
+		ValType *temp;
+		temp = new ValType[Elements + MAX_VECTOR_SIZE];
+		for (int i = 0; i < Elements; i++)
+			temp[i] = pMem[i];
+//		temp[Elements] = d;
+		delete[] pMem;
+		pMem = temp;
+		Elements += MAX_VECTOR_SIZE;
+	}
+	pMem[top] = d;
+	top += 1;
+}
+
+#endif
+
+
+
+
--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -2,3 +2,105 @@
 
 #include "stack.h"
 #include <gtest.h>
+TEST(Stack, can_create_stack_with_positive_length)
+{
+	ASSERT_NO_THROW(Stack<int> st(5));
+}
+
+TEST(Stack, cant_create_too_large_stack)
+{
+	ASSERT_ANY_THROW(Stack<int> st(MAX_VECTOR_SIZE + 1));
+}
+
+TEST(TStack, throws_when_create_stack_with_negative_length)
+{
+	ASSERT_ANY_THROW(Stack<int> st(-5));
+}
+
+
+TEST(Stack, can_not_delete_if_stack_is_empty)
+{
+	Stack<int> st(1);
+	//st.Push(1);
+	//st.Clean();
+	ASSERT_ANY_THROW(st.Eject());
+}
+
+TEST(Stack, can_not_see_the_top_element_if_stack_is_empty)
+{
+	Stack<int> st(1);
+	st.Push(1);
+	st.Clean();
+	ASSERT_ANY_THROW(st.Top());
+}
+
+
+
+TEST(Stack, can_get_quantity)
+{
+	Stack<int> st(10);
+	for (int i = 0; i<4; i++)
+		st.Push(i);
+
+	EXPECT_EQ(4, st.GetSize());
+}
+
+TEST(Stack, can_push_elem1)
+{
+	Stack<int> st(3);
+	st.Push(1);
+	st.Push(2);
+	st.Push(3);
+
+	ASSERT_NO_THROW(st.Push(4));
+}
+
+TEST(Stack, can_push_elem2)
+{
+	Stack<int> st(4);
+	st.Push(1);
+	st.Push(2);
+	st.Push(3);
+	st.Push(4);
+
+	ASSERT_NO_THROW(st.Push(4));
+}
+
+TEST(Stack, can_eject_elem)
+{
+	Stack<int> st(2);
+	st.Push(1);
+	st.Push(2);
+	
+	ASSERT_NO_THROW(st.Eject());
+}
+
+TEST(Stack, can_check_for_emptiness)
+{
+	Stack<int> st(2);
+	st.Push(1);
+	st.Push(2);
+	st.Clean();
+	EXPECT_EQ(true, st.IsEmpty());
+}
+
+TEST(Stack, can_check_for_completeness)
+{
+	Stack<int> st(2);
+	st.Push(1);
+	st.Push(2);
+	EXPECT_EQ(true, st.IsFull());
+}
+
+//TEST(Stack, can_return_the_number_of_elements)
+//{
+//	Stack<int> st(4);
+//	st.Push(1);
+//	st.Push(3);
+//	st.Push(1);
+//	st.Push(2);
+//	st.Clean();
+//
+//	EXPECT_EQ(true, st.IsEmpty());
+//}
+
